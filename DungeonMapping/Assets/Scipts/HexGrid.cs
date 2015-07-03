d748db1e962bb2d9fdfa2636b9f614c343ba2beb
Frazzlectra <baherndon16@tfa.edu>
2015-07-03 18:34:21 -0500

    Placing Hexes
    Added feature where you can click on a hex from the hud then left click
    on the map and it will replace the current hex with the new one, but I
    need to make it some the new one gets a sphere collider so it can be
    changed.

--- DungeonMapping/Assets/Scipts/HexGrid.cs
+++ DungeonMapping/Assets/Scipts/HexGrid.cs
@@ -20,11 +20,19 @@ public class HexGrid : MonoBehaviour {
     //hover effect
     int hexMask;
     public static string hoverHex;
+    //place Hex
+    public GameObject forrestHex;
+    public GameObject grassHex;
+    public GameObject waterHex;
+    public GameObject defaultHex;
 
     void Start()
     {
+        //when I get the main menu working this is how the user will set the size
         //_x = MainMenu.mapWidth;
         //_y = MainMenu.mapHeight;
+
+        //this is for creating the grid
         float unitLength = ( useAsInnerCircleRadius )? (radius / (Mathf.Sqrt(3)/2)) : radius;
         offsetX = unitLength * Mathf.Sqrt(3);
         offsetY = unitLength * 1.5f;
@@ -72,6 +80,10 @@ public class HexGrid : MonoBehaviour {
     void Update()
     {
         MouseHover();
+        if (MapHud.mouseFollower != null && Input.GetKey(KeyCode.Mouse0))
+        {
+            PlaceHex();
+        }
     }
     void MouseHover()
     {
@@ -82,6 +94,42 @@ public class HexGrid : MonoBehaviour {
             //Debug.Log("hit hex mask?" + hit.collider.name);
             hit.collider.gameObject.SendMessage("HoverAnimation");
             hoverHex = hit.collider.name;
+
+            if (MapHud.mouseFollower != null)
+            {  
+                MapHud.mouseFollower.transform.position = hit.point;
+            }
         }
+    }
+    private void PlaceHex()
+    {
+        string hexName = MapHud.mouseFollower.name;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, hexMask))
+        {
+            string hitName = hit.collider.name;//the hit colliders name I will change the instantiated name to this then delet hit collider then find it agian with this so I can add things
+            switch (hexName) //switch statement figures out which hex to instantiate based off of name of the mouse follower
+            {
+                case "forrestHex(Clone)":
+                    Instantiate(forrestHex, hit.collider.transform.position, hit.collider.transform.rotation).name = hit.collider.name;
+                    
+                    break;
+                case "grassHex(Clone)":
+                    Instantiate(grassHex, hit.collider.transform.position, hit.collider.transform.rotation).name = hit.collider.name;
+                    break;
+                case "waterHex(Clone)":
+                    Instantiate(waterHex, hit.collider.transform.position, hit.collider.transform.rotation).name = hit.collider.name;
+                    break;
+                case "defaultHex(Clone)":
+                    Instantiate(defaultHex, hit.collider.transform.position, hit.collider.transform.rotation).name = hit.collider.name;
+                    break;
+            }
+            Destroy(hit.collider.gameObject);
+            Debug.Log("hit name: " + hitName);
+            GameObject.Find(hitName).SendMessage("NewHex");
+        }
+
+
     }
 }
